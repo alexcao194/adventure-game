7bee87e464a5e3b921c1b49f90d1bd9435876976
Cao Việt Đức <alexcao194@gmail.com>
2023-09-30 14:12:38 +0700

    [Py-06] clean project(remove images, class sample),

--- src/configs/assets.py
+++ src/configs/assets.py
@@ -1,8 +1,4 @@
 from framework.framework import *
 
 class Assets(Singleton):
-    idle = 'assets/textures/idle/'
-    walk = 'assets/textures/walk/'
-    roll = 'assets/textures/roll/'
-    test = 'assets/textures/test/'
-    test2 = 'assets/textures/test2/'
+    pass
