008a8fdffd09874acf0f6945a763ae857b5ffe36
Cao Việt Đức <alexcao194@gmail.com>
2023-10-02 21:30:20 +0700

    [Py-06] animation_manager and texture will be checked in the first frame instead of initial time

--- framework/game/entity/entity.py
+++ framework/game/entity/entity.py
@@ -8,13 +8,9 @@ class Entity:
     position: Vector2 - the position of the entity
     hitbox: Vector2 - the size of the entity and the hitbox
     '''
-    def __init__(self, hitbox: Vector2 = None, position: Vector2 = None, animation_manager = None, texture = None):
+    def __init__(self, hitbox: Vector2 = None, position: Vector2 = None):
         if(self.__class__.__name__ == 'Entity'):
             raise Exception("Entity cannot be instantiated")
-        if(animation_manager == None and texture == None):
-            raise Exception("Entity must have either animation_manager or texture")
-        if(animation_manager != None and texture != None):
-            raise Exception("Entity cannot have both animation_manager and texture")
         if(position == None):
             raise Exception("Entity must have a position")
         if(hitbox == None):
@@ -30,6 +26,10 @@ class Entity:
         self.texture = texture
     
     def render(self, display):
+        if(animation_manager == None and texture == None):
+            raise Exception("Entity must have either animation_manager or texture")
+        if(animation_manager != None and texture != None):
+            raise Exception("Entity cannot have both animation_manager and texture")
         if(self.isActive == False):
             return
         if(self.animation_manager != None):
