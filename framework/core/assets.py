fa28b613fbfbcc5e5f74678724700f8c8163d973
Cao Việt Đức <alexcao194@gmail.com>
2023-10-04 07:47:43 +0700

    [Py-06] added check_box

--- framework/core/assets.py
+++ framework/core/assets.py
@@ -1,3 +1,5 @@
-class Assets:
+from framework.core.singleton import Singleton
+
+class Assets(Singleton):
     ic_blank_check_box = 'framework/assets/blank_check_box.png'
     ic_selected_check_box = 'framework/assets/selected_check_box.png'
