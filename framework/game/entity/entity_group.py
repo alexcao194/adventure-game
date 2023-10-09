6539c25dd03f18add280b96be5b8286f902a472d
Cao Việt Đức <alexcao194@gmail.com>
2023-10-09 23:04:53 +0700

    [Py-06] Collision system, layer sorting by hitbox

--- framework/game/entity/entity_group.py
+++ framework/game/entity/entity_group.py
@@ -7,6 +7,7 @@ class EntityGroup:
     entities: list - the list of entities
     '''
     def __init__(self):
+        self.sort_by_possition = True
         self.entities = []
     
     def add(self, entity: Entity):
@@ -19,5 +20,15 @@ class EntityGroup:
         for entity in self.entities:
             entity.__render__(display)
     def __update__(self, event):
+        if(self.sort_by_possition):
+            self.entities.sort(key=lambda entity: entity.rect.y)
+        
         for entity in self.entities:
+            collision_list = []
+            for other_entity in self.entities:
+                if(entity == other_entity):
+                    continue
+                if(entity.rect.colliderect(other_entity.rect)):
+                    collision_list.append(other_entity)
+            entity.__on_collision__(collision_list)
             entity.__update__(event)
