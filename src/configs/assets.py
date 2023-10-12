cdbfdda3689dd7f8888828fc12d4759ae710ad4e
Cao Việt Đức <alexcao194@gmail.com>
2023-10-12 16:52:52 +0700

    [Py-06] Player movement and actions

---
+++ src/configs/assets.py
@@ -0,0 +1,37 @@
+from framework.framework import *
+
+class Assets(Singleton):
+    # Animations
+    ani_attack_behind = 'assets/animations/hero/attack/attack_behind/'
+    
+    ani_attack_left = 'assets/animations/hero/attack/attack_left/'
+    
+    ani_attack_opposite = 'assets/animations/hero/attack/attack_opposite/'
+    
+    ani_attack_right = 'assets/animations/hero/attack/attack_right/'
+    
+    ani_death = 'assets/animations/hero/death/'
+    
+    ani_die = 'assets/animations/hero/die/'
+    
+    ani_run_behind = 'assets/animations/hero/run/run_behind/'
+    
+    ani_run_left = 'assets/animations/hero/run/run_left/'
+    
+    ani_run_opposite = 'assets/animations/hero/run/run_opposite/'
+    
+    ani_run_right = 'assets/animations/hero/run/run_right/'
+    
+    ani_walk_behind = 'assets/animations/hero/walk/walk_behind/'
+    
+    ani_walk_left = 'assets/animations/hero/walk/walk_left/'
+    
+    ani_walk_opposite = 'assets/animations/hero/walk/walk_opposite/'
+    
+    ani_walk_right = 'assets/animations/hero/walk/walk_right/'
+    
+    # Textures
+    tt_background = 'assets/textures/background/background.jpg'
+    
+    tt_demo = 'assets/textures/demo.jpg'
+    
